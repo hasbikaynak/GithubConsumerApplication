41d582eb8c2ba72751b53ad84610e1b7ee34c5a5
kaynak <kaynakhasbi1@gmail.com>
2023-09-05 17:28:03 +0200

    Project ConsumerApplication has been created

---
+++ src/main/java/com/example/githubconsumerapplication/GithubConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubconsumerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubconsumerapplication/GithubConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubconsumerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
