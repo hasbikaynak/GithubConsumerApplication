9be679e91dcd7bcecf700513e66e01f8255197c9
kaynak <kaynakhasbi1@gmail.com>
2023-09-07 12:09:39 +0200

    Refactored the code, added try catch blocks for custom exception mechanisms. Token has been deleted

--- src/main/java/com/example/githubconsumerapplication/config/AppConfig.java
+++ src/main/java/com/example/githubconsumerapplication/config/AppConfig.java
@@ -1,9 +1,7 @@
 package com.example.githubconsumerapplication.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
